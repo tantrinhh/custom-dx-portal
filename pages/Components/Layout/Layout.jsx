ea947c2c819493292f6ae105fdc485e5a76a667a
tantrinhh <tantrinh0322@gmail.com>
2023-09-18 23:32:27 +0700

    fix:tsx -> jsx

--- pages/Components/Layout/Layout.jsx
+++ pages/Components/Layout/Layout.jsx
@@ -1,16 +1,11 @@
-import Head from "next/head";
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-type LayoutPageProps = {
-  children?: React.ReactNode;
-};
-const LayoutPage = ({ children }: LayoutPageProps) => {
+const LayoutPage = ({ children }) => {
   return (
     <>
       <Header />
-
       {children}
       <Footer />
     </>
