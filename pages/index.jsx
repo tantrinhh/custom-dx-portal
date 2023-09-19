c8e9f7819c310ba9e13dabcad83040f11cc5c90c
tantrinhh <tantrinh0322@gmail.com>
2023-09-19 16:09:18 +0700

    ....

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,4 @@
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 const Home = () => {
-  const { push } = useRouter();
-  useEffect(() => {
-    push("/home");
-  }, []);
   return <></>;
 };
 
